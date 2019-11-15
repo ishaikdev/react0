ca5fe4c61d76ec1041de5cc44c5d794a0e042741
solmak <22554966+SolMak@users.noreply.github.com>
2019-11-15 17:42:17 -0500

    6

--- sick-fits/frontend/components/Page.js
+++ sick-fits/frontend/components/Page.js
@@ -1,19 +1,46 @@
 import React, { Component } from 'react';
+import styled, { ThemeProvider, injectGlobal } from 'styled-components';
 import Header from './Header';
 import Meta from './Meta';
 
-//This is the wrapper injected into _app.js which overrides default app from next
-// This component can be duplicated to create multiple themes
-// This is the default theme, and further refactoring will be needed to support multiple themes as hotswaps
+
+//styled-components provides theming via this theme variable
+// theme is an object with properties which can be reused around the app
+// this way these properties can be accessed on all props without manually drilling down props
+const theme = {
+  red: '#FF0000',
+  black: '#393939',
+  grey: '#3A3A3A',
+  lightgrey: '#E1E1E1',
+  offWhite: '#EDEDED',
+  maxWidth: '1000px',
+  bs: '0 12px 24px 0 rgba(0, 0, 0, 0.09)',
+};
+
+
+//wrapping one in the other and just wrapping, reconsider more better structuring
+const StyledPage = styled.div`
+  background: white;
+  color: ${props => props.theme.black};
+`;
+
+const Inner = styled.div`
+  max-width: ${props => props.theme.maxWidth};
+  margin: 0 auto;
+  padding: 2rem;
+`;
 
 class Page extends Component {
   render() {
     return (
-      <div>
-        <Meta />
-        <Header />
-        {this.props.children}
-      </div>
+      //ThemeProvider is coming from the styled_components themes functionality
+      <ThemeProvider theme={theme}>
+        <StyledPage>
+          <Meta />
+          <Header />
+          <Inner>{this.props.children}</Inner>
+        </StyledPage>
+      </ThemeProvider>
     );
   }
 }
